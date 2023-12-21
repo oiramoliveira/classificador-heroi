ffd90adee6b49a01a1c44438f5399b66c4aea24f
oiramlopes1 <oiramlopesdeoliveira@gmail.com>
2023-12-21 02:51:31 -0300

    Criado variavel para nome do herio e captura do teclado

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-asdasdgasdga
+const prompt = require('prompt-sync')({sigint: true})//Habilita receber valores do teclado
+
+
+let nomeHeroi //variável para armazenar o nome do herói
+
+nomeHeroi = prompt("Digite o nome do seu Herói: ")//aguarda usuário digitar nome do herói
+
