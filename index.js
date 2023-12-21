5e9593b3abd439fc09342f88814e6490933ca482
oiramlopes1 <oiramlopesdeoliveira@gmail.com>
2023-12-21 02:54:46 -0300

    Criação da variável de xp

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const prompt = require('prompt-sync')({sigint: true})//Habilita receber valores 
 
 
 let nomeHeroi //variável para armazenar o nome do herói
+let xp //variável para armazenar a experiência do herói
 
 nomeHeroi = prompt("Digite o nome do seu Herói: ")//aguarda usuário digitar nome do herói
 
